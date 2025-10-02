8bf8602100f565d91393d93078d6c9a369b3006d
cl2638 <145237887+cl2638@users.noreply.github.com>
2025-10-02 10:07:16 -0700

    Update bootstrap.js
    Update to Bootstrap file to also load header.html (for NavBar)

--- team_website/bootstrap.js
+++ team_website/bootstrap.js
@@ -1,6 +1,16 @@
+// Load header
+fetch('header.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('header').innerHTML = html;
+    })
+    .catch(err => console.error('Failed to load header:', err));
+
+// Load footer
 fetch('footer.html')
 .then(response => response.text())
 .then(html => {
     document.getElementById('footer').innerHTML = html;
 })
+
 .catch(err => console.error('Failed to load footer:', err));
