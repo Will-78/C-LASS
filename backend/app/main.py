be4a295a984740d15087812a3d7f05f2519007b7
Sean <seagol1010@gmail.com>
2025-12-01 14:08:33 -0700

    Edit prompt and add message history

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,9 @@ from neo4j import GraphDatabase
 from neo4j_graphrag.retrievers import VectorRetriever
 from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 from neo4j_graphrag.llm import OpenAILLM
-from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.generation import GraphRAG, RagTemplate
+from neo4j_graphrag.message_history import InMemoryMessageHistory
+from neo4j_graphrag.types import LLMMessage
 
 class Message(BaseModel):
     message: str
@@ -28,14 +30,46 @@ retriever = VectorRetriever(
 # Note: the OPENAI_API_KEY must be in the env vars
 llm = OpenAILLM(model_name="gpt-4o", model_params={"temperature": 0})
 
+# Create prompt template
+prompt_template = RagTemplate(
+    template='''
+    You are a helpful tutor. Give hints to help guide the user to the answer to their question. 
+    Do not outright give the answer. Do not make up answers.
+
+    Context:
+        {context}
+
+    Examples:
+        {examples}
+    
+    Question:
+        {query_text}
+        
+    Response:
+    ''',
+    expected_inputs=["context", "query_text", "examples"]
+)
+
 # Initialize the RAG pipeline
-rag = GraphRAG(retriever=retriever, llm=llm)
+rag = GraphRAG(retriever=retriever, llm=llm, prompt_template=prompt_template)
+
+# NOTE: this is assumes only a single session can be used
+history = InMemoryMessageHistory()
+
+message = LLMMessage(role="assistant", content="Hello!")
+history.add_message(message)
 
 app = FastAPI()
 
 @app.post("/test")
 def api_test(request: Message) -> Message:
+    message = LLMMessage(role="user", content=request.message)
+    history.add_message(message)
+
     # query the graph
-    response = rag.search(query_text=request.message, retriever_config={"top_k": 5})
+    response = rag.search(query_text=request.message, message_history=history, retriever_config={"top_k": 5})
+
+    message = LLMMessage(role="assistant", content=response.answer)
+    history.add_message(message)
 
     return Message(message = response.answer)
