fc5e86b9f3899514056578334e7ee39a965e0f9a
Sean <seagol1010@gmail.com>
2025-12-01 03:16:41 -0700

    set up VERY barebones chatbot that uses example GraphRAG system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from neo4j import GraphDatabase
+from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.generation import GraphRAG
+
+class Message(BaseModel):
+    message: str
+
+# Demo database credentials
+URI = "neo4j+s://demo.neo4jlabs.com"
+AUTH = ("recommendations", "recommendations")
+# Connect to Neo4j database
+driver = GraphDatabase.driver(URI, auth=AUTH)
+
+# retriever
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
+retriever = VectorRetriever(
+    driver,
+    index_name="moviePlotsEmbedding",
+    embedder=embedder,
+    return_properties=["title", "plot"],
+)
+
+# LLM
+# Note: the OPENAI_API_KEY must be in the env vars
+llm = OpenAILLM(model_name="gpt-4o", model_params={"temperature": 0})
+
+# Initialize the RAG pipeline
+rag = GraphRAG(retriever=retriever, llm=llm)
 
 app = FastAPI()
 
-@app.get("/test")
-def api_test():
-    return {"message": "Backend working!"}
+@app.post("/test")
+def api_test(request: Message) -> Message:
+    # query the graph
+    response = rag.search(query_text=request.message, retriever_config={"top_k": 5})
+
+    return Message(message = response.answer)
