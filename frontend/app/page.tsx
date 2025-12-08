d936b0b9225bd6bb50b926efa33bd0b07a796007
William Barnett <will892@LAPTOP-GCQMH64J>
2025-12-08 01:53:27 -0700

    Removed max off page.tsx until future implementation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import BackendTest from './components/backend-test';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="w-full max-w-[900px] scale-100">
+      <main className="w-full scale-100">
           <BackendTest />
         <div>
         </div>
